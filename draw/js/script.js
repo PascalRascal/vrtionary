d627726669efc2d63ca6c1206173c411ba0a5c2e
jmkuehn <kuehn.51@osu.edu>
2017-04-08 22:00:12 -0400

    Literally working canvas

---
+++ draw/js/script.js
@@ -0,0 +1,4 @@
+var lc
+  $(document).ready(function(){
+    lc = LC.init(document.getElementsByClassName('canvas core')[0], {primaryColor: '#ff0000', defaultStrokeWidth: 2, imageSize: {width: 732, height: 400}, backgroundColor: '#fff'});
+  });
