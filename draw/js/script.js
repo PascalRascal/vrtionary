f7eaeb54faf1e9fe320f50594cb35e953c8f2bc0
Blaise Marchetti <Blaise Marchetti>
2017-04-08 22:31:08 -0400

    Canvas Rescaling

--- draw/js/script.js
+++ draw/js/script.js
@@ -1,4 +1,20 @@
 var lc
+console.log('yaaaaaaaaa');
   $(document).ready(function(){
-    lc = LC.init(document.getElementsByClassName('canvas core')[0], {primaryColor: '#ff0000', defaultStrokeWidth: 2, imageSize: {width: 732, height: 400}, backgroundColor: '#fff'});
+    var canvasWidth = 720;
+    var canvasHeight = 480;
+    lc = LC.init(document.getElementsByClassName('canvas core')[0], {
+      primaryColor: '#ff0000', 
+      defaultStrokeWidth: 2, 
+      imageSize: {width: canvasWidth, height: canvasHeight}, 
+      backgroundColor: '#fff'});
+      
+      var cel = lc.containerEl;
+      if(cel.clientHeight > canvasHeight || cel.clientWidth > canvasWidth){
+        console.log("WOOOO");
+        console.log(lc.zoom);
+        lc.setZoom((canvasHeight/canvasWidth) / (cel.clientHeight/cel.canvasWidth));
+                console.log(lc.zoom);
+
+      }
   });
